ed2fa6a90a1e4054ee5433d8f1ea5ec5d948cb72
haven rutter <108587591+havenbeats1@users.noreply.github.com>
2022-09-23 12:54:08 -0700

    good to go

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -18,7 +18,7 @@ const ChannelCard = ({ channelDetail, marginTop}) => (
     marginTop,
   }}
     >
-      <Link to={`/channel${channelDetail?.id?.channelId}`}>
+      <Link to={`/channel/${channelDetail?.id?.channelId}`}>
         <CardContent sx={{ display: 'flex', flexDirection:
         'column', justifyContent: 'center', textAlign:
         'center', color: '#fff' }}>
