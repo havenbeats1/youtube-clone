10c5a6fe4dd56215a6e3367004faebd645fedca9
haven rutter <108587591+havenbeats1@users.noreply.github.com>
2022-09-23 09:38:18 -0700

    channelcard

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -4,11 +4,18 @@ import { Link } from 'react-router-dom';
 
 import { demoProfilePicture } from '../utils/constants';
 
-const ChannelCard = ({ channelDetail}) => (
+const ChannelCard = ({ channelDetail, marginTop}) => (
   <Box 
   sx={{
     boxShadow: 'none',
     borderRadius: '20px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: { xs: '356px', md: '320px'},
+    height: '326px',
+    margin: 'auto',
+    marginTop,
   }}
     >
       <Link to={`/channel${channelDetail?.id?.channelId}`}>
@@ -26,7 +33,12 @@ const ChannelCard = ({ channelDetail}) => (
             <CheckCircle sx={{ fontSize: 14, color:
                 'gray', ml: '5px' }} />
           </Typography>
-              
+              {channelDetail?.statistics?.subscriberCount && (
+                <Typography>
+                  {parseInt(channelDetail?.statistics?.
+                  subscriberCount).toLocaleString()} Subscribers
+                </Typography>
+              )}
         </CardContent>
       </Link>
   </Box>
--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import { Videos, ChannelCard } from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const ChannelDetail = () => {
+  const [channelDetail, setChannelDetail] = useState(null)
+  const [videos, setVideos] = useState([])
+
+  const { id } = useParams();
+
+  console.log(channelDetail, videos)
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`)
+    .then((data) => setChannelDetail(data?.items[0]));
+
+    fetchFromAPI(`search?channelId=${id}&part=snippet&
+    order=date`)
+    .then((data) => setVideos(data?.items));
+  }, [id])
+
   return (
-    <div>ChannelDetail</div>
+    <Box minHeight="95vh">
+      <Box>
+        <div style={{
+          background: 'linear-gradient(90deg, rgba(63,94,251,1) 0%, rgba(252,70,107,1) 100%)',
+          zIndex: 10,
+        }}
+        />
+        <ChannelCard channelDetail={channelDetail} 
+        marginTop="-110px"/>
+      </Box>
+      <Box p={2} display="flex">
+        <Box sx={{ mr: { sm: '100px' } }} />
+          <Videos videos={videos} />
+        </Box>
+      </Box>
   )
 }
 
